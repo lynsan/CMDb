6aa36770b91adf0ab1ef35e15bf0e0f455bdce83
Pilvi <pilviborjesson@gmail.com>
2020-10-30 10:35:50 +0100

    Making readmore btns, and making poster layout for bigger and smaller screens.

--- ProjMovie/wwwroot/js/main.js
+++ ProjMovie/wwwroot/js/main.js
@@ -12,3 +12,20 @@ accordionItemHeaders.forEach(accordionItemHeader => {
         }
     });
 });
+
+function ReadMore(ev) {
+    const btn = ev.target;
+    const dots = btn.parentElement.closest('.movie-plot').querySelector('.dots');
+    const moreText = btn.parentElement.closest('.movie-plot').querySelector('.more');
+    const btnText = btn.parentElement.closest('.movie-plot').querySelector('.myBtn');
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
