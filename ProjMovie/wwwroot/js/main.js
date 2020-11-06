90795899f6cd32e7d433d9fe134d1d8d41fb3b10
Lyn Santiago Enriquez <lysa1900@student.miun.se>
2020-11-06 09:41:07 +0100

    fixed accordion AGAIN

--- ProjMovie/wwwroot/js/main.js
+++ ProjMovie/wwwroot/js/main.js
@@ -106,21 +106,6 @@ function getApiDislike() {
         .catch((err) => console.log(err))
 }
 
-function ShowInfo() {
-    const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-    accordionItemHeaders.forEach(accordionItemHeader => {
-        accordionItemHeader.addEventListener("click", event => {
-            accordionItemHeader.classList.toggle("active");
-            const accordionItemBody = accordionItemHeader.nextElementSibling;
-            if (accordionItemHeader.classList.contains("active")) {
-                accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-            }
-            else {
-                accordionItemBody.style.maxHeight = 0;
-            }
-        });
-    });
-}
 
 
